8bdc64535a89193ef7ad2c69846e914bfab4ec1f
Nife <nife0612@gmail.com>
2023-03-06 19:15:04 +0400

    Wait and Notify

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,22 @@ public class Main {
         // first();
         // second();
         // secondByLambda();
-
+        // third();
+        thirdByInterrupter();
     }
 
     private static void first() throws InterruptedException {
+        int countOfThreads = 22;
+        ArrayList<Thread> threadsList = new ArrayList<>();
+
         System.out.println("Main thread started...");
 
-        ArrayList<Thread> t = new ArrayList<Thread>();
-        int num = 22;
-        for (int i = 0; i < num; i++) {
-            t.add(new JThread("JThread" + i));
-            t.get(i).start();
+        for (int i = 0; i < countOfThreads; i++) {
+            threadsList.add(new JThread("JThread" + i));
+            threadsList.get(i).start();
         }
 
-        for (Thread thread : t) {
+        for (Thread thread : threadsList) {
             thread.join();
         }
 
@@ -49,9 +51,35 @@ public class Main {
         System.out.println("Main thread finished...");
     }
 
+    private static void third(){
+        System.out.println("Main started...");
+        MyThread thread = new MyThread();
+        new Thread(thread, "MyThread").start();
 
-    public static void name(Thread tre){
-        System.out.println(tre.getName());
+        try {
+            Thread.sleep(1100);
+            thread.disable();
+            Thread.sleep(1000);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Main finished... \n");
+    }
+
+    private static void thirdByInterrupter(){
+        System.out.println("Main thread started...");
+        MyJThread thread = new MyJThread("MyJThread");
+        thread.start();
+
+        try {
+            Thread.sleep(150);
+            thread.interrupt();
+            Thread.sleep(150);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Main thread finished...");
     }
 
 }
@@ -85,5 +113,56 @@ class MyRun implements Runnable{
         }
         System.out.printf("%s finished...\n", Thread.currentThread().getName());
 
+    }
+}
+
+class MyThread implements Runnable{
+    private boolean isActive;
+
+    MyThread(){
+        isActive = true;
+    }
+
+    public void disable(){
+        isActive = false;
+    }
+    @Override
+    public void run() {
+        System.out.printf("%s started... \n", Thread.currentThread().getName());
+        int counter = 1;
+        while (isActive){
+            System.out.println("Loop: " + counter++);
+            try{
+                Thread.sleep(400);
+            } catch (InterruptedException e) {
+                System.out.println("Thread has been interrupted");
+            }
+        }
+        System.out.printf("%s finished... \n", Thread.currentThread().getName());
+    }
+}
+
+class MyJThread extends Thread{
+    MyJThread(String name){
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("%s started... \n", Thread.currentThread().getName());
+
+        int counter = 1;
+
+        try {
+            while(!isInterrupted()) {
+                System.out.println("Loop: " + counter++);
+                Thread.sleep(30);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(Thread.currentThread().getName() + " has been interrupted");
+        }
+
+
+        System.out.printf("%s finished... \n", Thread.currentThread().getName());
     }
 }
---
+++ src/Synchronization.java
@@ -0,0 +1,101 @@
+public class Synchronization {
+    public static void main(String[] args){
+        //first();
+        //second();
+
+    }
+
+
+    private static void first() {
+        CommonResource commonResource = new CommonResource();
+        for (int i = 1; i < 6; i++){
+            Thread thread = new Thread(new CountThread(commonResource));
+            thread.setName("Thread" + i);
+            thread.start();
+        }
+    }
+
+    private static void second() {
+        CommonResource res = new CommonResource();
+        for (int i = 1; i < 6; i++){
+            Thread thread = new Thread(new SynchronizedRunCountThread(res));
+            thread.setName("SynchThread " + i);
+            thread.start();
+        }
+    }
+
+}
+
+class CommonResource{
+    int x = 0;
+}
+
+class SynhCommonResource{
+    int x = 0;
+
+    synchronized void increment(){
+        x = 1;
+        for (int i = 1; i < 5; i++){
+            System.out.printf("%s %d \n", Thread.currentThread().getName(), x++);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {}
+
+        }
+    }
+}
+
+class CountThread implements Runnable{
+    CommonResource res;
+    CountThread(CommonResource res){
+        this.res = res;
+    }
+
+    @Override
+    public void run() {
+        res.x = 1;
+        for (int i = 1; i < 5; i++){
+            System.out.printf("%s %d \n", Thread.currentThread().getName(), res.x++);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                System.out.printf("%s is interrupt \n", Thread.currentThread().getName());
+            }
+        }
+    }
+}
+
+class SynchronizedRunCountThread implements Runnable{
+    final CommonResource res;
+    SynchronizedRunCountThread(CommonResource res){
+        this.res = res;
+    }
+
+    @Override
+    public void run() {
+        synchronized (res){
+            res.x = 1;
+            for (int i = 1; i < 5; i++) {
+                System.out.printf("%s %d \n", Thread.currentThread().getName(), res.x++);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    System.out.printf("%s is interrupted \n", Thread.currentThread().getName());
+                }
+            }
+        }
+    }
+}
+
+class SynchCountThread implements Runnable{
+    SynhCommonResource res;
+    SynchCountThread(SynhCommonResource res){
+        this.res = res;
+    }
+
+
+    @Override
+    public void run() {
+        res.increment();
+    }
+}
